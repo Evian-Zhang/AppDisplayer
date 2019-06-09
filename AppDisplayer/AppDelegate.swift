131edd3e2ce3282dddff88eb4dc00b3a981a6619
Evian-Zhang <evianzhang1999@163.com>
2019-06-09 14:30:03 +0800

    Initial Commit

---
+++ AppDisplayer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AppDisplayer
+//
+//  Created by Evian张 on 2019/6/9.
+//  Copyright © 2019 Evian张. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
